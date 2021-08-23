7a7a19d2ced07b35a4549c3b10bab5dfed7297a2
bruno.gianesi <bruno.gianesi@ifood.com.br>
2021-08-23 09:57:51 -0300

    Add MFCCs features

--- CETUC_Feature_extractor_paralelizes.py
+++ CETUC_Feature_extractor_paralelizes.py
@@ -8,25 +8,30 @@ import time
 import nest_asyncio
 from speechpy import feature
 
-async def LMFE(count, file):
+async def extract_MFCCs(count, file):
     async with sem:
         file_path = os.path.join(current_path,"CETUC", "Full", file)
+        
+        audio_data, sample_rate = librosa.load(file_path)
+        
+        mfccs = librosa.feature.mfcc(y=audio_data, sr=sample_rate)
+        
+        mfccs_mean = list(np.mean(mfccs.T, axis= 0))
         if file[0] == 'F':
             gender = 0
         if file[0] == 'M': 
             gender = 1
-        audio_data, sample_rate = librosa.load(file_path, sr=16000)
         
-        logenergy = feature.lmfe(audio_data, sampling_frequency=sample_rate,
-                                    frame_length=0.020, frame_stride=0.01,
-                                    num_filters=40, fft_length=512,
-                                    low_frequency=0, high_frequency=None)
-
-        features_list.append([file, logenergy, gender])
-                                    
-        print(f"\r{count}/{len(audio_files)}", end='')
-
-        return
+        sample_features = mfccs_mean
+        sample_features.insert(0,str(file))
+        sample_features.append(gender)
+        print(sample_features)
+        await asyncio.wait(1)
+        #print(f"\r{count}/{len(audio_files)}",end='')
+        features_list.append(sample_features)
+        
+        
+        return 
 
 
 # #Calculo de tempo de disparo
@@ -36,7 +41,7 @@ start_time = time.time()
 loop = asyncio.get_event_loop()
 
 #Controle de requisições por vez
-sem = asyncio.Semaphore(3000)
+sem = asyncio.Semaphore(600)
 
 #Array de tasks
 sents = []
@@ -53,49 +58,19 @@ current_path = os.getcwd()
 file_path = os.path.join(current_path,"CETUC", "Full")
 audio_files = os.listdir(file_path)
 
-for k, file in enumerate(audio_files[:50000]):
-    sent = asyncio.ensure_future(LMFE(count=k+1, file=file))
-    #sent = asyncio.create_task(LMFE(count=k+1, file=audio_files))
+for k, file in enumerate(audio_files[:5]):
+    sent = asyncio.ensure_future(extract_MFCCs(count=k+1, file=file))
     
     sents.append(sent)
  
 done, _ = loop.run_until_complete(asyncio.wait(sents))
 
 
-dataframe_features = pd.DataFrame(features_list, columns = ['FileName', 'LMFE', 'Gender'])
-dataframe_features.to_csv('data/CETUC_LMFE_data.csv', index=False)
+dataframe_features = pd.DataFrame(features_list, columns = ['FileName',
+                                                            'MFCC_1',  'MFCC_2',  'MFCC_3',  'MFCC_4',  'MFCC_5',
+                                                            'MFCC_6',  'MFCC_7',  'MFCC_8',  'MFCC_9',  'MFCC_10',
+                                                            'MFCC_11',  'MFCC_12',  'MFCC_13',  'MFCC_14',  'MFCC_15',
+                                                            'MFCC_16',  'MFCC_17',  'MFCC_18',  'MFCC_19',  'MFCC_20',
+                                                            'Gender'])
 
-
-
-
-
-# #Calculo de tempo de disparo
-start_time = time.time()
-
-#inicio do Loop
-loop = asyncio.get_event_loop()
-
-#Controle de requisições por vez
-sem = asyncio.Semaphore(3000)
-
-#Array de tasks
-sents = []
-
-nest_asyncio.apply()
-
-#Coleta as recomendações para envio
-gender_list = []
-file_list = []
-features_list = []
-
-for k, file in enumerate(audio_files[50000:]):
-    sent = asyncio.ensure_future(LMFE(count=k+1, file=file))
-    #sent = asyncio.create_task(LMFE(count=k+1, file=audio_files))
-    
-    sents.append(sent)
- 
-done, _ = loop.run_until_complete(asyncio.wait(sents))
-
-
-dataframe_features = pd.DataFrame(features_list, columns = ['FileName', 'LMFE', 'Gender'])
-dataframe_features.to_csv('data/CETUC_LMFE_data_2.csv', index=False)
+dataframe_features.to_csv('data/CETUC_MFCCs_data.csv', index=False)
